ae89eb77a34534efde7cfaa9acceeae75fec8a75
P3rXeuS <vanegaivannoya@gmail.com>
2024-12-19 11:30:36 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Sesuaikan dengan file sumber proyekmu
   ],
   theme: {
     extend: {},
